9b62fa8fbbf4e2e9a81a4bb9b26e1859a050360c
James Wang <jamesw96@uw.edu>
2017-08-22 15:55:21 -0700

    Command Line User Input Enabled
    Typo Fix

--- FlightSearcher.py
+++ FlightSearcher.py
@@ -1,13 +1,35 @@
 import urllib.request
 import json
 
-API_KEY = "<your API key>"
+API_KEY = "<your API Key>"
 
 def main():
-	### origin & destination -> use IATA code
-	### dates use String in the format of yyyy-mm-dd
-	### pass in two date for round trip results
-    search_flight("JFK", "SEA", 1, "2017-09-13", "2017-09-15")
+    ### origin & destination -> use IATA code
+    ### dates use String in the format of yyyy-mm-dd
+    ### pass in two date for round trip results
+    origin = input("Origin? ")
+    destination = input("Destination? ")
+    round_trip = input("Round trip (Y/N)? ")
+    departing_year = input("Outbounding year? ")
+    departing_month = parse_input(input("Outbounding month? "))
+    departing_day = parse_input(input("Outbounding day? "))
+
+    departure_date = str(departing_year) + "-" + str(departing_month) + "-" + str(departing_day)
+    returning_date = None
+
+    if round_trip == "Y":
+        returning_year = input("Inbounding year? ")
+        returning_month = parse_input(input("Inbounding month? "))
+        returning_day = parse_input(input("Inbounding day? "))
+        returning_date = str(returning_year) + "-" + str(returning_month) + "-" + str(returning_day)
+        search_flight(origin, destination, 1, departure_date, returning_date)
+    else:
+        search_flight(origin, destination, 1, departure_date)
+
+def parse_input(input_string: str):
+    if len(input_string) < 2:
+        return "0" + input_string
+    return input_string
 
 def search_flight(origin: str, destination: str, num: int, departure_date: str, return_date=None):
     request_string = "http://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?"
@@ -20,6 +42,7 @@ def search_flight(origin: str, destination: str, num: int, departure_date: str, 
     result_num = "number_of_results=" + str(num) + "&"
 
     request_string = request_string + origin_string + destination_string + d_date + r_date + result_num + "apikey=" + API_KEY
+    print(request_string)
     req = urllib.request.Request(request_string)
     file = urllib.request.urlopen(req)
     json_data = json.loads(file.read())
